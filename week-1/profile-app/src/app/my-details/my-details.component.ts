584cdab6c8bf9c889f5f60298cb772261ebfbb52
William John Austin <wiaustin@my365.bellevue.edu>
2023-08-12 18:50:49 -0700

    Adding Exercise 1.5

---
+++ week-1/profile-app/src/app/my-details/my-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-details',
+  templateUrl: './my-details.component.html',
+  styleUrls: ['./my-details.component.css']
+})
+export class MyDetailsComponent {
+
+}
---
+++ week-1/profile-app/src/app/my-image/my-image.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-image',
+  templateUrl: './my-image.component.html',
+  styleUrls: ['./my-image.component.css']
+})
+export class MyImageComponent {
+
+}
