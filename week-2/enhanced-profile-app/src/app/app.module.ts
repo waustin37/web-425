dd4be4f2f967fab93f5785daa32645736d874f5a
William John Austin <wiaustin@my365.bellevue.edu>
2023-08-19 14:51:15 -0400

    Adding Exercise 2.3

---
+++ week-2/enhanced-profile-app/src/app/app.module.ts
@@ -0,0 +1,27 @@
+/**
+ * Title: app.module.ts
+ * Author: William Austin
+ * Date: 12 August 2023
+ * Description: App module
+ */
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { MyImageComponent } from './my-image/my-image.component';
+import { MyDetailsComponent } from './my-details/my-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyImageComponent,
+    MyDetailsComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
