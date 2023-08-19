ec3b0e994b22c2d95f3dd504e0edea9c1860827c
William John Austin <wiaustin@my365.bellevue.edu>
2023-08-19 15:59:05 -0400

    Adding Exercise 2.4

---
+++ week-2/composer-app/src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ComposerListComponent } from './composer-list/composer-list.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    component: ComposerListComponent
+  },
+  {
+    path:'composerList',
+    component: ComposerListComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ week-2/composer-app/src/app/composer-list/composer-list.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+export default class Composer {
+  fullName: string;
+  genre: string;
+
+  constructor(fullName:string, genre: string){
+    this.fullName = fullName;
+    this.genre = genre
+  }
+}
+
+@Component({
+  selector: 'app-composer-list',
+  templateUrl: './composer-list.component.html',
+  styleUrls: ['./composer-list.component.css']
+})
+export class ComposerListComponent {
+  composers: Array<Composer>;
+  constructor(){
+    this.composers = [
+      new Composer("Brain Eno", "Ambient"),
+      new Composer("Philip Glass", "Neo-Classical"),
+      new Composer("Lady Gaga", "Pop"),
+      new Composer("John Williams", "Soundtrack"),
+      new Composer("Lin Manuel Miranda", "Musical")
+    ]
+  }
+}
